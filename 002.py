ecbe0e896369a61cd6a51b2f62f10cb338c8fbb7
matus <matus.jan.lavko@infinitycapital.sk>
2021-05-05 10:11:21 +0200

    progress

--- 002.py
+++ 002.py
@@ -1,10 +1,12 @@
 #brute force method
 def solve():
     fib = [1,2]
-    while sum(fib) < 4e6:
+    while fib[-1] < 4e6:
         x_t = fib[-1] + fib[-2]
         fib.append(x_t)
-    answer = sum(fib)
+
+    even = [x for x in fib if x % 2 == 0]
+    answer = sum(even)
     print(str(answer))
 
 if __name__ == '__main__':
